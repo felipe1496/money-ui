dcf7baeefb0d2f1e400a8d1e7b9d765e7173568e
felipe1496 <felipe.oliveira1496@gmail.com>
2026-03-05 21:15:26 -0300

    commit

--- src/components/AsyncSelectCategory.tsx
+++ src/components/AsyncSelectCategory.tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getCategoriesQueryOpts } from '../queries/categories-queries';
 import type { CategoriesService } from '../services/CategoriesService';
 import { useDebounce } from '../hooks/useDebounce';
+import { SaveCategoryDialog } from '../pages/categories/components/SaveCategoryDialog';
+import { usePostCategory } from '../hooks/mutations/usePostCategory';
 
 interface Props {
   selected?: Option<
@@ -14,10 +16,12 @@ interface Props {
       Awaited<ReturnType<typeof CategoriesService.getCategories>>['data']['categories'][number]
     > | null,
   ) => void;
+  isCreatable?: boolean;
 }
 
-export const AsyncSelectCategory: FC<Props> = ({ selected = null, onChange }) => {
+export const AsyncSelectCategory: FC<Props> = ({ selected = null, onChange, isCreatable }) => {
   const [search, setSearch] = useState<string>('');
+  const [isCreating, setIsCreating] = useState('');
 
   const debouncedSearch = useDebounce(search, 500);
 
@@ -33,15 +37,32 @@ export const AsyncSelectCategory: FC<Props> = ({ selected = null, onChange }) =>
       })),
   });
 
+  const { mutateAsync: createCategory } = usePostCategory();
+
   return (
-    <AsyncSelect
-      options={data ?? []}
-      isLoading={isFetching}
-      search={search}
-      selected={selected}
-      onSelectedChange={onChange}
-      onSearchChange={setSearch}
-      placeholder="Select a category..."
-    />
+    <>
+      <AsyncSelect
+        options={data ?? []}
+        isLoading={isFetching}
+        search={search}
+        selected={selected}
+        onSelectedChange={onChange}
+        onSearchChange={setSearch}
+        placeholder="Select a category..."
+        onCreate={(value) => {
+          setIsCreating(value);
+        }}
+      />
+      {isCreatable && isCreating && (
+        <SaveCategoryDialog
+          isVisible={!!isCreating}
+          onSave={(data) => {
+            createCategory(data);
+          }}
+          onVisibleChange={() => setIsCreating('')}
+          defaultValues={{ name: isCreating, color: '' }}
+        />
+      )}
+    </>
   );
 };
